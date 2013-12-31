00a02187c3629727490372de689befcb1241fdc9
Parker Miller <despondentdonkey@gmail.com>
2013-12-31 16:41:13 +0000

    Switched to PDO instead of using raw sql functions.

--- register.php
+++ register.php
@@ -1,30 +1,29 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '', 'db_test');
+try {
+    $sql = new PDO('mysql:host=localhost;dbname=db_test', 'root', '');
+    $sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Default is ERRMODE_SILENT. I believe the current option will allow for exceptions to be called and handled.
+} catch(PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
 
 $uname = $_POST['username'];
 $pass = $_POST['password'];
 
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-function usernameExists($con, $name) {
-    $unames = mysqli_query($con, "SELECT username FROM users");
-    while ($row = mysqli_fetch_array($unames)) {
-        $currentName = $row[0];
-        if ($currentName == $name) {
+function usernameExists($sql, $name) {
+    $data = $sql->query("SELECT * FROM users");
+    foreach ($data as $row) {
+        if ($row['username'] == $name) {
             return true;
         }
     }
     return false;
 }
 
-if (usernameExists($con, $uname)) {
+if (usernameExists($sql, $uname)) {
     echo 'Username already exists.';
 } else {
-    mysqli_query($con, "INSERT INTO users (username, password) VALUES('" . $uname . "', '" . $pass . "')");
+    $statement = $sql->prepare("INSERT INTO users (username, password) VALUES(:uname, :pass);");
+    $statement->execute(array('uname'=>$uname, 'pass'=>$pass));
     echo 'Registered';
 }
-
 ?>
